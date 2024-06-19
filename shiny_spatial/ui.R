60275b65284626f6ea02927a24e741c83354129d
ZhengTianyu2021 <zhengtianyunc@gmail.com>
2024-06-19 09:25:05 +0200

    Shiny UI transcript and label

--- shiny_spatial/ui.R
+++ shiny_spatial/ui.R
@@ -8,6 +8,30 @@
 #
 
 library(shiny)
+# Creeate a parameter tabs
+# gene_name <- rownames(HD)
+# Seurat_cluster <- HD@meta.data$seurat_cluster.projected
+# Banksy_cluster <- HD@meta.data$banksy_cluster
+
+
+# parameter_tabs <- tabsetPanel(
+#   id = "params",
+#   type = "hidden",
+#   tabPanel("Transcript",
+#            selectInput("gene_name", "Gene name", choices = rownames(HD)
+#   ),
+#   tabPanel("SeuratCluster", 
+#            selectInput("Seurat_cluster", "Seurat")
+#   ),
+#   tabPanel("BanksyCluster",
+#            numericInput("rate", "rate", value = 1, min = 0),
+#   ),
+#   tabPanel("Celltype",
+#            numericInput("rate", "rate", value = 1, min = 0),
+#   )
+# )
+
+
 
 # Define UI for application that draws a histogram
 fluidPage(
@@ -18,9 +42,9 @@ fluidPage(
     # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
-          selectInput("dataSelect", "Choose a Dataset", choices = c("Mouse Brain" = "hd_mouse_brain.rds")),
+          selectInput("HD_disply", "Disply", selected = "Label", choices = c("Transcript", "Label")),
+          uiOutput("ui"),
           actionButton("loadData", "Load Data"),
-          
         ),
 
         # Show a plot of the generated distribution
@@ -29,3 +53,7 @@ fluidPage(
         )
     )
 )
+
+
+
+
