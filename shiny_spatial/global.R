c88024f09ac28b0e809f5379e4dce391e0096bed
mengxiao-he <mengxiao.he@scilifelab.se>
2024-06-19 09:59:22 +0200

    update global.R with path

--- shiny_spatial/global.R
+++ shiny_spatial/global.R
@@ -1,5 +1,7 @@
 # global.R
 
+set_data_path <- "/Users/mengxiaohe/10x_Visium_HD_data/mouse_brain/"
+
 # Check if 'Seurat' package is loaded and load it if necessary
 if (!require(Seurat, quietly = TRUE)) {
   library(Seurat)
@@ -7,5 +9,5 @@ if (!require(Seurat, quietly = TRUE)) {
 
 # Check if 'mb_hd' is already loaded to avoid reloading
 if (!exists("mb_hd")) {
-  mb_hd <- readRDS("path/to/your/initial_file.rds")
+  mb_hd <- readRDS(file = paste0(set_data_path, "seurat_hd_mb_banksy_rctd.rds"))
 }
